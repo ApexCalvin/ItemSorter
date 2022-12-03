bdb7f7c9ad92a32e196c41f3e73a2bf5fabfb348
Calvin <calvin@Zipcoders-MacBook-Pro.local>
2022-12-03 08:32:43 -0500

    completed

--- src/main/java/rocks/zipcode/io/comparators/IdComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/IdComparator.java
@@ -1,7 +1,26 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+import java.util.Objects;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class IdComparator {
+public class IdComparator implements Comparator<Item>{
+    @Override
+    public int compare(Item o1, Item o2) {
+        //positive, is the first item passed bigger than the second time passed?
+
+//        if(o1.getId() > o2.getId()) {
+//            return 1;
+//        } else if (o1.getId() < o2.getId()) {
+//            return -1;
+//        } else if (Objects.equals(o1.getId(), o2.getId())){
+//            return 0;
+//        }
+
+        return o1.getId().compareTo(o2.getId());
+    }
 }
--- src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
@@ -1,7 +1,25 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class PriceComparator {
+public class PriceComparator implements Comparator<Item> {
+    @Override
+    public int compare(Item o1, Item o2) {
+        //positive, is the first item passed bigger than the second time passed?
+
+//        if(o1.getId() > o2.getId()) {
+//            return 1;
+//        } else if (o1.getId() < o2.getId()) {
+//            return -1;
+//        } else if (Objects.equals(o1.getId(), o2.getId())){
+//            return 0;
+//        }
+
+        return o1.getPrice().compareTo(o2.getPrice());
+    }
 }
